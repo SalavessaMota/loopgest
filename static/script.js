e0d03b85b506112ddd9046ace3cdbf9dec38f4dd
SalavessaMota <141644814+SalavessaMota@users.noreply.github.com>
2023-09-18 17:09:58 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -13,26 +13,49 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
+
+function adjustRows() {
+  var messageTextarea = document.getElementById('message');
+  var addressTextarea = document.getElementById('address');
+
+  var windowWidth = window.innerWidth;
+
+  if (windowWidth <= 768) {
+    messageTextarea.rows = "1"; 
+    addressTextarea.rows = "1";
+  } else {
+    messageTextarea.rows = "4"; 
+    addressTextarea.rows = "4";
+  }
+}
+
+
+window.addEventListener('resize', adjustRows);
+
+
+window.addEventListener('load', adjustRows);
+
+
 function sendEmail(event){
   event.preventDefault();
   Email.send({
-      SecureToken : "2aafbf0c-936c-482e-bc34-994c06e5ac80",
+      SecureToken: "c616706f-a186-4e50-8a7f-a48dc939c2e7",
       To : 'nunosalavessamota@gmail.com',
       From : 'nunosalavessamota@gmail.com',
       Subject : "Novo pedido de proposta",
       Body : "Nome: " + document.getElementById("name").value
-        + "<br> Email: " + document.getElementById("email").value
-        + "<br> Telefone: " + document.getElementById("phone").value
-        + "<br> Tipologia: " + document.getElementById("message").value
-        + "<br> Morada: " + document.getElementById("address").value
+          + "<br> Email: " + document.getElementById("email").value
+          + "<br> Telefone: " + document.getElementById("phone").value
+          + "<br> Tipologia: " + document.getElementById("message").value
+          + "<br> Morada: " + document.getElementById("address").value
   }).then(
-    message => {
-      console.log(message);
-      if(message === 'OK'){
-        alert("Obrigado pelo seu contacto");
-      } else {
-        alert("Houve um erro ao enviar o email: " + message);
+      message => {
+          console.log("SMTP.js says: " + message);
+          if(message === 'OK'){
+              alert("Obrigado pelo seu contacto");
+          } else {
+              alert("Houve um erro ao enviar o email: " + message);
+          }
       }
-    }
   );
 }
